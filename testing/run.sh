47ea4d83806d16fd732c1e50bf9b229cc1f3ce75
justicedong <justicedong@naver.com>
2021-12-06 13:09:46 +0900

    Add: test logic

--- testing/run.sh
+++ testing/run.sh
@@ -1,21 +1,24 @@
-participants="1"
+participants="3"
+duration="1000"
+conferences="1"
+jitsiUrl="meet.jit.si"
 
-docker-compose -f './jitsi-meet-torture/docker-compose.yaml' up --build --scale chrome="$participants" -d
+docker-compose up --build --scale chrome="$participants" -d
 
-mvn -f './jitsi-meet-torture/pom.xml' \
+mvn -f 'jitsi-meet-torture/pom.xml' \
       -Dthreadcount=1 \
-      -Dorg.jitsi.malleus.conferences=1 \
+      -Dorg.jitsi.malleus.conferences="$conferences" \
       -Dorg.jitsi.malleus.participants="$participants" \
       -Dorg.jitsi.malleus.senders="$participants" \
       -Dorg.jitsi.malleus.audio_senders="$participants" \
       -Dorg.jitsi.malleus.duration="$duration" \
-      -Dorg.jitsi.malleus.room_name_prefix=loadtest-"$i" \
+      -Dorg.jitsi.malleus.room_name_prefix=testtest \
       -Dorg.jitsi.malleus.regions="" \
       -Dremote.address="http://localhost:4444/wd/hub" \
       -Djitsi-meet.tests.toRun=MalleusJitsificus \
       -Dwdm.gitHubTokenName=jitsi-jenkins \
       -Dremote.resource.path='/usr/share/jitsi-meet-torture' \
-      -Djitsi-meet.instance.url="$jitsiUrl" \
+      -Djitsi-meet.instance.url="https://meet.jit.si" \
       -Djitsi-meet.isRemote=true \
       -Dchrome.disable.nosanbox=true \
-      test &>/dev/null &
+      test
