e2adbf34b6a7fdbba98df840fbfb4adfdfb74610
justicedong <justicedong@naver.com>
2021-12-07 15:36:38 +0900

    Add: testing logic & development file

--- testing/run.sh
+++ testing/run.sh
@@ -1,24 +1,28 @@
-participants="3"
-duration="1000"
-conferences="1"
-jitsiUrl="meet.jit.si"
+participants=8
+duration=1000
+jitsiUrl=https://peopleundercloud.com
 
-docker-compose up --build --scale chrome="$participants" -d
+docker-compose -f 'jitsi-meet-torture/docker-compose.yml' down
+
+docker-compose -f 'jitsi-meet-torture/docker-compose.yml' up --build --scale chrome="$participants" -d
 
 mvn -f 'jitsi-meet-torture/pom.xml' \
       -Dthreadcount=1 \
-      -Dorg.jitsi.malleus.conferences="$conferences" \
+      -Dorg.jitsi.malleus.conferences=1 \
       -Dorg.jitsi.malleus.participants="$participants" \
       -Dorg.jitsi.malleus.senders="$participants" \
       -Dorg.jitsi.malleus.audio_senders="$participants" \
       -Dorg.jitsi.malleus.duration="$duration" \
-      -Dorg.jitsi.malleus.room_name_prefix=testtest \
-      -Dorg.jitsi.malleus.regions="" \
+      -Dorg.jitsi.malleus.join_delay=100 \
+      -Dorg.jitsi.malleus.max_disrupted_bridges_pct=0 \
+      -Dorg.jitsi.malleus.room_name_prefix=loadtest \
       -Dremote.address="http://localhost:4444/wd/hub" \
       -Djitsi-meet.tests.toRun=MalleusJitsificus \
       -Dwdm.gitHubTokenName=jitsi-jenkins \
       -Dremote.resource.path='/usr/share/jitsi-meet-torture' \
-      -Djitsi-meet.instance.url="https://meet.jit.si" \
+      -Djitsi-meet.instance.url="$jitsiUrl" \
       -Djitsi-meet.isRemote=true \
       -Dchrome.disable.nosanbox=true \
+      -DallowInsecureCerts=true \
+      --debug \
       test
