9a7b8ed0dad80fc15363455699c987226c3b7aae
justicedong <justicedong@naver.com>
2021-12-13 01:25:34 +0900

    Add: update logic

--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 echo "run update"
 kubectl patch statefulset shard-0-jvb --type='json' -p='[{"op":"replace", "path":  "/spec/template/spec/containers/0/image", value: "justicedong/jvb:0.0.1"}]' -n jitsi
 echo "update config"
-# kubectl exec -it pod/shard-0-web-57dd4cb8c8-vnb4h -n jitsi -- echo "JVB_NOW_UPDATING=true" >> /config/config.js -n jitsi
+kubectl exec -it pod/shard-0-web-57dd4cb8c8-vnb4h -n jitsi -- echo "JVB_NOW_UPDATING=true" >> /config/config.js -n jitsi
 
-sleep 1
+sleep 10
 
 echo "update finish"
-# kubectl exec -it pod/shard-0-web-57dd4cb8c8-vnb4h -n jitsi -- echo "JVB_NOW_UPDATING=false" >> /config/config.js -n jitsi
+kubectl exec -it pod/shard-0-web-57dd4cb8c8-vnb4h -n jitsi -- echo "JVB_NOW_UPDATING=false" >> /config/config.js -n jitsi
 
 echo "change config"
 echo "done..."
